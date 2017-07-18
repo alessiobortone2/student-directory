bf2a0543e817fa0397f8b3b5b4cedbd2ddbd7e4d
alessiobortone2 <alessiobortone2@gmail.com>
2017-07-18 12:36:58 +0100

    added write to file section

--- directory.rb
+++ directory.rb
@@ -30,6 +30,7 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save students to file"
   puts "9. Exit"
 end
 
@@ -52,11 +53,23 @@ def process(selection)
       input_students
     when "2"
       show_students
+    when "3"
+      save_students
     when "9"
       exit
     else
       puts "Not sure what you mean, please try again"
   end
+end
+
+def save_students
+  file = File.open("students.csv", "w")
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
 end
 
 interactive_menu
